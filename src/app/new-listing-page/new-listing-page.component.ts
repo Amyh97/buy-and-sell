d004babbd770d0ba304d6537e98b0e36fc4bfe16
Amyh97 <amy.hollis@btinternet.com>
2023-04-06 16:54:16 +0100

    create add/edit form and handle actions separately

--- src/app/new-listing-page/new-listing-page.component.ts
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { fakeListings } from '../fake-data';
-import { Listing } from '../types';
 
 @Component({
   selector: 'app-new-listing-page',
@@ -9,9 +7,6 @@ import { Listing } from '../types';
   styleUrls: ['./new-listing-page.component.css']
 })
 export class NewListingPageComponent implements OnInit {
-  name: string = '';
-  description: string = '';
-  price: string = '';
   constructor(private router: Router){}
 
   ngOnInit(): void {
