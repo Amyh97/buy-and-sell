db8051649b2d17e511354a4813aedad9090f89e2
Amyh97 <amy.hollis@btinternet.com>
2023-04-06 16:09:45 +0100

    create new listings page

--- src/app/new-listing-page/new-listing-page.component.ts
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-new-listing-page',
   templateUrl: './new-listing-page.component.html',
   styleUrls: ['./new-listing-page.component.css']
 })
-export class NewListingPageComponent {
+export class NewListingPageComponent implements OnInit {
+  name: string = '';
+  description: string = '';
+  price: string = '';
+  constructor(private router: Router){}
 
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit():void {
+    alert("Creating a new listing...");
+    this.router.navigateByUrl('/my-listings');
+  }
 }
